293f1fea32a68947d43b2ef78a57abbd6c2a8e44
Keegan Bowen <keegan.bowen@gmail.com>
2014-12-03 11:12:59 -0700

    Update date-pipe.sh

--- date-pipe.sh
+++ date-pipe.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+################ An alarm clock for unix using sec.pl
+# Keegan Bowen 
+####################################################$
+
+# Keep the thing running as often as we can with this:
 traprestart()
 {
 $0 "$$" &
@@ -6,6 +12,7 @@ exit 0
 }
 trap traprestart HUP TERM INT
 
+# Main loop which sends the output of date to a named pipe.
 while true; do 
 date > ~/sec-alarm/date.pipe
 sleep 1
