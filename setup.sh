eae765036644f9d9b1cb6db9ec1b84609da18784
Keegan Bowen <keegan.bowen@gmail.com>
2014-12-03 11:13:48 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget http://hyalitepress.com/alarm.wav
 mkdir ~/sec-alarm
+mkfifo ~/sec-alarm/date.pipe
 cp ./alarm.sh ~/sec-alarm/
 cp ./alarm.wav ~/sec-alarm/
 cp ./alarm-clock.cfg ~/sec-alarm/
