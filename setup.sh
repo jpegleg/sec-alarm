a14ee7c610423228a96e06ec8debb59c2112cd26
Keegan Bowen <keegan.bowen@gmail.com>
2016-01-27 21:40:08 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ mkdir ~/sec-alarm
 mkfifo ~/sec-alarm/date.pipe
 cp ./alarm.sh ~/sec-alarm/
 cp ./alarm.wav ~/sec-alarm/
-cp ./alarm-clock.cfg ~/sec-alarm/
+cp ./sec-alarm.cfg ~/sec-alarm/
 cp ./run-alarm.sh ~/sec-alarm/
 cp ./setup.sh ~/sec-alarm/
