8e35d8582f7803ed1437255991a52e797c69c616
Keegan Bowen <keegan.bowen@gmail.com>
2016-01-27 21:57:58 -0700

    Update stop-alarm.sh

--- stop-alarm.sh
+++ stop-alarm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Stop the sec-alarm
-for y in $(ps auxwww | grep ".input...sec.alarm.date.pipe" | grep -v grep | awk '{print $2}'); do 
-  kill -9 "$x"; 
-done
-for x in $(ps aux | grep date-pipe.sh | grep -v grep | awk '{print $2}'); do 
-  kill -9 "$x"; 
+ps auxwww | grep ".input...sec.alarm.date.pipe" | grep -v grep | awk '{print $2}' | xargs kill -9
+for x in $(ps aux | grep date-pipe.sh | grep -v grep | awk '{print $2}'); do
+  kill -9 "$x";
 done
